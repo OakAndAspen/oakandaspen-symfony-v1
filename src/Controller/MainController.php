26268d6079372a5d27d3febd2f0c6e21ab974b1b
Irina Despot <irinadespot@gmail.com>
2019-02-14 02:29:22 +0100

    Navigation and Contact page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,10 +8,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
+     * @Route("/", name="blog")
+     */
+    public function blog()
+    {
+        return $this->render('main/blog.html.twig', []);
+    }
+
+    /**
      * @Route("/timeline", name="timeline")
      */
     public function timeline()
     {
         return $this->render('main/timeline.html.twig', []);
+    }
+
+    /**
+     * @Route("/work", name="work")
+     */
+    public function work()
+    {
+        return $this->render('main/work.html.twig', []);
+    }
+
+    /**
+     * @Route("/life", name="life")
+     */
+    public function life()
+    {
+        return $this->render('main/life.html.twig', []);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('main/contact.html.twig', []);
     }
 }
